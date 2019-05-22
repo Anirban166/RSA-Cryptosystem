8db66c589b299feeeaee7432dfb4880b8fb784cf
Anirban166 <bloodraven166@gmail.com>
2019-05-22 21:57:26 +0530

    Euclid's Extended GCD algo implementation.

---
+++ RSA_euclidgcd.py
@@ -0,0 +1,14 @@
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = egcd(b % a, a)
+        return g, x - (b // a) * y, y
+
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
