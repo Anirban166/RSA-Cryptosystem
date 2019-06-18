d04367221cd2c253866ee4dbf4c236cdb249fa35
Anirban166 <bloodraven166@gmail.com>
2019-06-18 14:42:28 +0530

    Double-Add algorithm
    Implementation of a simple double-add algorithm. (just in case needed)

---
+++ DAA.py
@@ -0,0 +1,25 @@
+def bits(n):
+    """
+    Generates the binary digits of n, starting
+    from the least significant bit.
+
+    bits(151) -> 1, 1, 1, 0, 1, 0, 0, 1
+    """
+    while n:
+        yield n & 1
+        n >>= 1
+
+def double_and_add(n, x):
+    """
+    Returns the result of n * x, computed using
+    the double and add algorithm.
+    """
+    result = 0
+    addend = x
+
+    for bit in bits(n):
+        if bit == 1:
+            result += addend
+        addend *= 2
+
+    return result
