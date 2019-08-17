091fde71c4db9cc6f9ee378ebfdf5fba73e4eb97
Anirban Chetia <bloodraven166@gmail.com>
2019-08-17 11:57:59 +0530

    RSA implementation in Java

---
+++ RSA.java
@@ -0,0 +1,78 @@
+/*
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Arrays;
+*/ //Required frameworks if using Standard library package methods.
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.math.BigInteger;
+import java.security.SecureRandom;    
+
+public class RSA {
+   private final static BigInteger one      = new BigInteger("1");
+   private final static SecureRandom random = new SecureRandom();
+
+   private BigInteger privateKey;
+   private BigInteger publicKey;
+   private BigInteger modulus;
+
+   // generate an N-bit (roughly) public and private key
+   RSA(int N) {
+      BigInteger p = BigInteger.probablePrime(N/2, random);
+      BigInteger q = BigInteger.probablePrime(N/2, random);
+      BigInteger phi = (p.subtract(one)).multiply(q.subtract(one));
+
+      modulus    = p.multiply(q);                                  
+      publicKey  = new BigInteger("65537");     // common value in practice = 2^16 + 1
+      privateKey = publicKey.modInverse(phi);
+   }
+
+
+   BigInteger encrypt(BigInteger message) {
+      return message.modPow(publicKey, modulus);
+   }
+
+   BigInteger decrypt(BigInteger encrypted) {
+      return encrypted.modPow(privateKey, modulus);
+   }
+
+   public String toString() {
+      String s = "";
+      s += "public  = " + publicKey  + "\n";
+      s += "private = " + privateKey + "\n";
+      s += "modulus = " + modulus;
+      return s;
+   }
+ 
+   public static void main(String[] args) {
+      int N = Integer.parseInt(args[0]);
+      RSA key = new RSA(N);
+      StdOut.println(key);
+ 
+      // create random message, encrypt and decrypt
+      BigInteger message = new BigInteger(N-1, random);
+
+      //// create message by converting string to integer
+      // String s = "test";
+      // byte[] bytes = s.getBytes();
+      // BigInteger message = new BigInteger(bytes);
+
+      BigInteger encrypt = key.encrypt(message);
+      BigInteger decrypt = key.decrypt(encrypt);
+      StdOut.println("message   = " + message);
+      StdOut.println("encrypted = " + encrypt);
+      StdOut.println("decrypted = " + decrypt);
+   }
+}
